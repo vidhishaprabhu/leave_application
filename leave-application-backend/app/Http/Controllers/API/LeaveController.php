7399b44e91c00f5105e74317baf2fcc5561dac04
vidhishaprabhu <vidhishaprabhu@gmail.com>
2025-04-22 14:18:57 +0530

    Added the store function and added the logic for store function and also added the routes for store in api.php file

--- leave-application-backend/app/Http/Controllers/API/LeaveController.php
+++ leave-application-backend/app/Http/Controllers/API/LeaveController.php
@@ -2,13 +2,35 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use App\Models\Leave;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class LeaveController extends Controller
 {
     public function index(){
-        
+        $leaves=Leave::where('user_id',Auth::id())->get();
+        return response()->json($leaves);
     }
-    
+    public function store(Request $request){
+        $request->validate([
+            'leave_type'=>'required|string',
+            'start_date'=>'required|date',
+            'end_date'=>'required|date',
+            'reason'=>'required|string',
+        ]);
+        $leave=Leave::create([
+            'user_id'=>Auth::id(),
+            'leave_type'=>$request->leave_type,
+            'start_date'=>$request->start_date,
+            'end_date'=>$request->end_date,
+            'reason'=>$request->reason,
+            'status'=>'Pending',
+        ]);
+        return response()->json([
+            'message'=>'Leave Application submitted successfully',
+            'leave'=>$leave
+        ]);        
+    }
 }
