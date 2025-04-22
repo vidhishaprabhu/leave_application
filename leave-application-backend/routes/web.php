64a6a4b1115e9802c7e3dd703b6eac51f50b3e36
vidhishaprabhu <vidhishaprabhu@gmail.com>
2025-04-22 12:53:45 +0530

    Added the login route in web.php file

--- leave-application-backend/routes/web.php
+++ leave-application-backend/routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/login', function () {
+    return response()->json(['message' => 'Please login'], 401);
+})->name('login');
