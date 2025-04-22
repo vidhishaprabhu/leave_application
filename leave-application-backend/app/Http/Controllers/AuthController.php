9e3fbcd408e3b53b8faac561f7a7e163b3d353d9
vidhishaprabhu <vidhishaprabhu@gmail.com>
2025-04-22 14:41:00 +0530

    Replace the file destination

--- leave-application-backend/app/Http/Controllers/AuthController.php
+++ leave-application-backend/app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
@@ -12,7 +13,7 @@ class AuthController extends Controller
         $request->validate([
             'name'=>'required|string|max:255',
             'email'=>'required|email|unique:users',
-            'password'=>'required|password|min:6'
+            'password'=>'required|string|min:6'
         ]);
         $user = User::create([
             'name'=>$request->name,
@@ -28,7 +29,23 @@ class AuthController extends Controller
 
     }
     public function login(Request $request){
-        
-
+        $request->validate([
+            'email'=>'required',
+            'password'=>'required'
+        ]);
+        if(!Auth::attempt($request->only('email','password'))){
+            return response()->json(['message'=>'Invalid Credentials'],401);
+        }
+        $user=Auth::user();
+        $token=$user::createToken('auth_token')->plainTextToken;
+        return reponse()->json([
+            'access_token'=>$token,
+            'token_type'=>'Bearer',
+            'user'=>$user
+        ]);
+    }
+    public function logout(Request $request){
+        $request->user()->tokens()->delete();
+        return response()->json(['message'=>'User loggged out successfully']);
     }
 }
