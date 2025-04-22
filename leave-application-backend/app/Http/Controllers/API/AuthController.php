83de631432aa016fb7f3df770f4107a10dc6c3f6
vidhishaprabhu <vidhishaprabhu@gmail.com>
2025-04-22 12:21:59 +0530

    Added the logic for both success as well s failure of login in login function of AuthController

--- leave-application-backend/app/Http/Controllers/API/AuthController.php
+++ leave-application-backend/app/Http/Controllers/API/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
@@ -28,7 +29,15 @@ class AuthController extends Controller
 
     }
     public function login(Request $request){
-        
-
+        $request->validate([
+            'email'=>'required',
+            'password'=>'required'
+        ]);
+        if(!Auth::attempt($request->only('email','password'))){
+            return response()->json(['message'=>'Invalid Credentials'],401);
+        }
+        $user=Auth::user();
+        $token=User::createToken('auth_token')->plainTextToken;
+        return reponse()->json(['message'=>'Login is done successfully'],201);
     }
 }
